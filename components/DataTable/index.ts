4aa42e950b9b3d5a3b1f181def18aa0f043f885c
Calista Mateuszczyk <calista.teus@gmail.com>
2026-04-08 18:44:09 -0500

    Add components/DataTable/index.ts

---
+++ components/DataTable/index.ts
@@ -0,0 +1,4 @@
+export { DataTable } from './DataTable';
+export type { Row, ColumnDef, GroupByConfig, DataTableProps, SortDirection, SortState, EditState, BreakpointName, BreakpointConfig } from './DataTable.types';
+export { generateMockData } from './mockData';
+export { useBreakpoint } from './useBreakpoint';
