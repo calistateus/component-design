186675ec47f85d6312983c394639d752bd7cddb6
Calista Mateuszczyk <calista.teus@gmail.com>
2026-04-08 20:29:16 -0500

    Fix storybook config

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
 import { registerRootComponent } from 'expo';
-const storybookEnabled = process.env.EXPO_PUBLIC_STORYBOOK_ENABLED === 'true';
-const Root = storybookEnabled
-  ? require('./.storybook/index').default
-  : require('./App').default;
-registerRootComponent(Root);
+import StorybookUIRoot from './.storybook/index';
+registerRootComponent(StorybookUIRoot);
