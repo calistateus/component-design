dba9264d0894979e9b75181ee7ebe67c7008bb2a
Calista Mateuszczyk <calista.teus@gmail.com>
2026-04-08 20:49:36 -0500

    Remove .storybook/index.js

--- .storybook/index.js
+++
@@ -1,9 +0,0 @@
-import { getStorybookUI } from '@storybook/react-native';
-
-import './storybook.requires';
-
-const StorybookUIRoot = getStorybookUI({
-  initialSelection: { kind: 'Design System/DataTable', name: 'Default' },
-});
-
-export default StorybookUIRoot;
