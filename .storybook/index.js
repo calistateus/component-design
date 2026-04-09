d34fd7392742889eab70a6f5d3b62c1db3abae22
Calista Mateuszczyk <calista.teus@gmail.com>
2026-04-08 20:29:15 -0500

    Fix storybook config

--- .storybook/index.js
+++ .storybook/index.js
@@ -1,2 +1,9 @@
-import { view } from '@storybook/react-native';
-export default view;
+import { getStorybookUI } from '@storybook/react-native';
+
+import './storybook.requires';
+
+const StorybookUIRoot = getStorybookUI({
+  initialSelection: { kind: 'Design System/DataTable', name: 'Default' },
+});
+
+export default StorybookUIRoot;
